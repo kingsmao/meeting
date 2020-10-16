6f918f2cc19108554f1febf1c5fd917ac2435eea
qinxuan <yiqinxuan@chainup.com>
2020-10-16 19:26:48 +0800

    feat(meeting): 增加健康检查接口

---
+++ src/main/java/com/chainup/action/HealthController.java
@@ -0,0 +1,20 @@
+package com.chainup.action;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author QX
+ * @date 2020/10/16
+ */
+@Slf4j
+@RestController
+public class HealthController {
+
+    @GetMapping("/health")
+    public String health(){
+        log.info("OOOOOOOOOOOKKKKKKKKKKKKKKKKKKK");
+        return "你瞅啥？";
+    }
+}
